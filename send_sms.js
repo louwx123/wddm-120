b0718da814d4693787219a4be503fa96975c1567
Weixiang Lou <weixianglou@WeixiangdeMacBook-Pro.local>
2020-02-29 23:48:04 -0500

    sms test

---
+++ send_sms.js
@@ -0,0 +1,11 @@
+const accountSid = 'AC884e7df64033bb9277e31e4f922964ac';
+const authToken = '30dbb86e83da55f9e6508925f0d44e23';
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+14043692027',
+     to: '+14169964509'
+   })
+  .then(message => console.log(message.sid));
